e95cf1f0b6124006057196305046166ed108954b
Dave <djtbdanner@yahoo.com>
2017-12-23 11:02:23 -0600

    add store

--- app/app.jsx
+++ app/app.jsx
@@ -3,6 +3,17 @@ var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require ('react-router');
 var TodoApp = require('TodoApp');
 
+var action = require('actions');
+var store = require('configureStore').configure();
+
+store.subscribe(() => {
+  console.log("New State", store.getState());
+});
+
+store.dispatch(action.addTodo("Clean the yard"));
+store.dispatch(action.setSearchText("yard"));
+store.dispatch(action.toggleShowCompleted());
+
 $(document).foundation();
 
 // application styles
