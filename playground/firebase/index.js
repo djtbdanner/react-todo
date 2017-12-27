7902083af8e80683b472cef74d6442af74d647d8
Dave Danner <djtbdanner@yahoo.com>
2017-12-27 13:29:45 -0600

    more playground on firebase

--- playground/firebase/index.js
+++ playground/firebase/index.js
@@ -11,11 +11,80 @@ var config = {
 firebase.initializeApp(config);
 var firebaseRef = firebase.database().ref();
 // app object name and version
-firebaseRef.set({app:{name:"Todo App", version: 1}}).then(() => {
-  console.log('Set Worked!');
-}, (e) => {
-  console.log('Set Failed!');
-})
+firebaseRef.set({
+  app: {
+    name: "Todo App",
+    version: "1.0.0"
+  },
+  isRunning: true,
+  user: {
+    name: "Dave Danner",
+    age: 53
+  }
+}); //.then(() => {
+//   console.log('Set Worked!');
+// }, (e) => {
+//   console.log('Set Failed!');
+// })
 
- //firebaseRef.child('app').child('name').set('Mike');
- firebaseRef.child('app').set({name: "Todo application"});
+/// challenge us .on to listen for changes in user object. The update user name to a differnt name, then update apps name see
+ firebaseRef.child('app').on('value', (snapshot) => {
+   console.log('Data changed', snapshot.val());
+ });
+
+console.log('updating app name');
+firebaseRef.update({'app/name': 'Todo Application'});
+
+console.log('updating user name');
+firebaseRef.update({'user/name': 'Some User'});
+
+// fetch data
+// firebaseRef.child('app').once('value').then((snapshot) => {
+//   console.log('Got entire datbase', snapshot.key, snapshot.val());
+// }, (e) => {
+//   console.log('unable to fetch value!', e);
+// });
+
+
+
+// // data listener
+// firebaseRef.on('value', (snapshot) => {
+//   console.log('Got value', snapshot.val());
+// });
+// // turn listener off
+// // firebaseRef.off();
+// firebaseRef.update({isRunning: false});
+
+
+
+// -- examples
+
+//firebaseRef.child('app').child('name').set('Mike');
+//firebaseRef.child('app').set({name: "Todo application"});
+
+/// multipath update
+// firebaseRef.update({isRunning: false, 'app/name': 'Todo Application'});
+//
+// firebaseRef.child('app').update({name: 'My Todo Application!'}).then(() => {
+//   console.log('update Worked!');
+// }, (e) => {
+//   console.log('update Failed!');
+// })
+
+// challenge - multiplat updat app name and user name in one call
+// firebaseRef.update({
+//   'app/name' : 'Todo application (mpath)',
+//   'user/name' : 'User name'
+// });
+
+// challenget update using child with two calls
+// firebaseRef.child('app').update({name:"todo application via child update"});
+// firebaseRef.child('user').update({name:"user name via child update"});
+
+//firebaseRef.remove();
+//firebaseRef.child('app/name').remove();
+//firebaseRef.child('app').update({version: '2.0.0', name : null});
+
+// challenge remove isRunning age by setting to null - remove age by calling remove
+// firebaseRef.update({isRunning:null});
+// firebaseRef.child('user/age').remove();
